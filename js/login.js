95a9010cb22a5c72f960d59e162db13f8ff78272
DaviGFiguer <davigarciafigueiredo@gmail.com>
2023-10-19 13:56:31 -0300

    Ajustes de códigos, mudança para objeto JSON

--- js/login.js
+++ js/login.js
@@ -2,16 +2,22 @@ $( document ).ready(function() {
   
 
   $("#form-login").submit(function(){
-    let usuario = $("#campo-login").val();
-    let senha = $("#senha").val();
-    if(usuario.trim() == "" || usuario == null){
+
+    usuario = {
+      campoUsuario : $("#campo-login").val(),
+      senha : btoa($("#senha").val())
+    }
+
+    console.log(usuario.senha)
+    
+    if(usuario.campoUsuario.trim() == "" || usuario.campoUsuario == null){
        bootbox.alert({
         message: 'O campo usuário não deve ser vazio!',
         backdrop: true       
         }).find('.modal-content').css({'background-color': '#2D2D2F', 'font-weight' : 'bold', color: '#FFFFFF', 'font-size': '2em'});
 
     }
-    else if(senha.trim() == "" || senha == null){
+    else if(usuario.senha.trim() == "" || usuario.senha == null){
       bootbox.alert({
         message: 'O campo senha não deve ser vazio!',
         backdrop: true
@@ -19,8 +25,14 @@ $( document ).ready(function() {
         
     }
     else{
+      /*bootbox.alert({
+        message: 'Bem vindo usuário ' + usuario.campoUsuario,
+        backdrop: true
+        }).find('.modal-content').css({'background-color': '#2D2D2F', 'font-weight' : 'bold', color: '#FFFFFF', 'font-size': '2em'});
+        */
+      alert('Bem vindo de volta, ' + usuario.campoUsuario);
       $(location).attr('href', 'chat.html');
-
+      
     }
     return false;
 
